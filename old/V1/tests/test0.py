8ebccd3259a650f2fb2297a5e66dacd1adebc1db
Thibault Plouviez <thibault.plouviez@icloud.com>
2020-12-18 19:43:56 +0100

    Remove old versions files and tests files

--- old/V1/tests/test0.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from tkinter import *
-import tkinter as tk
-from tkinter import messagebox
-
-print ('BIENVENUE, élève')
--- old/VF/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from tkinter import *
-import tkinter as tk
-from tkinter import messagebox
-from fn_main import *
-import fn_main as fmn
-
-
-fmn.accmain()
